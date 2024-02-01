b905d61da207c905aa1429bf431fec3dcc743d25
Scott Block <scottrblock@gmail.com>
2024-02-01 00:29:16 -0500

    quick wip

--- script/script.py
+++ script/script.py
@@ -4,7 +4,7 @@ import argparse
 
 
 def extract_links(directory):
-    accumulated_link_sets = {"arxiv_pdf": set(), "arxiv_abs": set(), "pdf": set()}
+    all_links = set()
 
     for root, _, files in os.walk(directory):
         for file in filter(lambda f: f.endswith(".md"), files):
@@ -12,48 +12,27 @@ def extract_links(directory):
             try:
                 with open(file_path, "r", encoding="utf-8") as f:
                     file_content = f.read()
-                new_link_sets = _extract_links_from_text(file_content)
-                accumulated_link_sets = _merge_link_sets(accumulated_link_sets, new_link_sets)
+                links = _extract_links_from_text(file_content)
+                all_links.update(links)
             except UnicodeDecodeError:
                 print(f"Unicode decode error in file {file_path}")
             except Exception as e:
                 print(f"Error reading file {file_path}: {e}")
 
-    return (
-        accumulated_link_sets["pdf"],
-        accumulated_link_sets["arxiv_pdf"],
-        accumulated_link_sets["arxiv_abs"],
-    )
+    return all_links
 
 
 def _extract_links_from_text(text):
-    link_sets = {"arxiv_pdf": set(), "arxiv_abs": set(), "pdf": set()}
-
     link_regex = re.compile(
-        r"(https?://(?:www\.)?arxiv\.org/(?:abs|pdf)/[^\s/#?]+(?:v\d+)?)(?=\W|$)|(https?://[^\s]+\.pdf)(?=\W|$)"
+        r"https?://(?:www\.)?arxiv\.org/[^\s]+(?=\W|$)|" r"https?://[^\s]+\.pdf(?=\W|$)"
     )
 
     try:
-        links = link_regex.findall(text)
-        for link_group in links:
-            link = next(sublink for sublink in link_group if sublink)
-            if "arxiv.org/pdf/" in link:
-                link_sets["arxiv_pdf"].add(link)
-            elif "arxiv.org/abs/" in link:
-                link_sets["arxiv_abs"].add(link)
-            elif link.endswith(".pdf"):
-                link_sets["pdf"].add(link)
+        return set(link_regex.findall(text))
     except Exception as e:
         snippet = text[:100] + "..." if len(text) > 100 else text
         print(f"Error processing text: {snippet}\nException: {e}")
-
-    return link_sets
-
-
-def _merge_link_sets(accumulated_link_sets, new_link_sets):
-    for key in accumulated_link_sets:
-        accumulated_link_sets[key] = accumulated_link_sets[key].union(new_link_sets[key])
-    return accumulated_link_sets
+        return set()
 
 
 def parse_arguments():
@@ -66,11 +45,8 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    pdf_links, arxiv_pdf_links, arxiv_abs_links = extract_links(args.directory)
-
-    print("PDF Links:", pdf_links)
-    print("ArXiv PDF Links:", arxiv_pdf_links)
-    print("ArXiv Abstract Links:", arxiv_abs_links)
+    all_links = extract_links(args.directory)
+    print("Extracted Links:", all_links)
 
 
 if __name__ == "__main__":
--- script/test_script.py
+++ script/test_script.py
@@ -5,37 +5,30 @@ from script import _extract_links_from_text
 def test_extract_links_ez():
     text = "Links: https://arxiv.org/pdf/1234.5678v1 https://arxiv.org/abs/1234.5678 https://example.com/document.pdf "
     expected = {
-        "arxiv_pdf": {"https://arxiv.org/pdf/1234.5678v1"},
-        "arxiv_abs": {"https://arxiv.org/abs/1234.5678"},
-        "pdf": {"https://example.com/document.pdf"},
+        "https://arxiv.org/pdf/1234.5678v1",
+        "https://arxiv.org/abs/1234.5678",
+        "https://example.com/document.pdf",
     }
+
     assert _extract_links_from_text(text) == expected
 
 
 def test_extracts_from_normal_markdown():
     url2 = "https://example.com/document.pdf"
     text = f"Check these links with markdown [named link]({url2}) is great"
-    expected = {
-        "arxiv_pdf": set(),
-        "arxiv_abs": set(),
-        "pdf": {url2},
-    }
-    assert _extract_links_from_text(text) == expected
+    assert _extract_links_from_text(text) == {url2}
 
 
 def test_extract_links_with_no_links():
     text = "This is a text without any links."
-    expected = {"arxiv_pdf": set(), "arxiv_abs": set(), "pdf": set()}
-    assert _extract_links_from_text(text) == expected
+    assert _extract_links_from_text(text) == set()
 
 
 def test_extract_links_with_invalid_links():
     text = "Invalid links: http://example.com, https://notarxiv.org/pdf/1234.5678"
-    expected = {"arxiv_pdf": set(), "arxiv_abs": set(), "pdf": set()}
-    assert _extract_links_from_text(text) == expected
+    assert _extract_links_from_text(text) == set()
 
 
 def test_extract_links_with_empty_string():
     text = ""
-    expected = {"arxiv_pdf": set(), "arxiv_abs": set(), "pdf": set()}
-    assert _extract_links_from_text(text) == expected
+    assert _extract_links_from_text(text) == set()
